ac2648d6ff560111a9558ff9af22fe938148a68a
RedCat17 <you@example.com>
2026-06-02 11:55:39 +0300

    Update Readme

--- examples/hello.asm
+++ examples/hello.asm
@@ -5,13 +5,13 @@ msg:
 .STR "Hello world!"
 
 start:
-MOV R0, msg ; load msg address to reg 0
+MOV R0, msg         ; load msg address to reg R0
 
 loop:
-LOADB R1, [R0] ; load byte from memory to reg 1
-STORB R1, [0xF801] ; send byte to TX address
-INC R0 ; increment pointer
-CMP R0, start ; test if we reached end of string
+LOADB R1, [R0]      ; load byte from memory to R1
+STORB R1, [0xF801]  ; send byte to TX address
+INC R0              ; increment pointer
+CMP R0, start       ; test if we reached end of string
 JNZ loop
 
-HLT
+HLT                 ; stop execution
--- readme.md
+++ readme.md
@@ -40,6 +40,26 @@ Redirecting debug output to file:
 ./akvm program.bin 2> output.txt
 ```
 
+## Code example
+"Hello world" written in Assembly for AK-VM:
+```
+JMP start
+msg:
+.STR "Hello world!"
+
+start:
+MOV R0, msg         ; load msg address to reg R0
+
+loop:
+LOADB R1, [R0]      ; load byte from memory to R1
+STORB R1, [0xF801]  ; send byte to TX address
+INC R0              ; increment pointer
+CMP R0, start       ; test if we reached end of string
+JNZ loop
+
+HLT                 ; stop execution
+```
+
 ## Documentation
 
 See:
