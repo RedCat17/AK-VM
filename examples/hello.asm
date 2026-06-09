638f1bd5a28d8bdd8656b05854d12bf71a639e2d
RedCat17 <140415791+RedCat17@users.noreply.github.com>
2026-06-09 12:34:58 +0300

    Update hello.asm

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,17 +1,17 @@
 ; This program outputs "Hello world!" 
 ; It tests memory, I/O, loops, labels
 JMP start
-msg:
-.STR "Hello world!"
+
+msg: .STR "Hello world!"
 
 start:
-MOV R0, msg         ; load msg address to reg R0
+    MOV R0, msg         ; load msg address to reg R0
 
-loop:
-LOADB R1, [R0]      ; load byte from memory to R1
-STORB R1, [0xF801]  ; send byte to TX address
-INC R0              ; increment pointer
-CMP R0, start       ; test if we reached end of string
-JNZ loop
+    loop:
+        LOADB R1, [R0]      ; load byte from memory to R1
+        STORB R1, [0xF801]  ; send byte to TX address
+        INC R0              ; increment pointer
+        CMP R0, start       ; test if we reached end of string
+        JNZ loop
 
-HLT                 ; stop execution
+    HLT                 ; stop execution
